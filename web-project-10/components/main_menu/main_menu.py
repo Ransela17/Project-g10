214b9ce262f4ba689f70cb7defd302aebb7eac40
Ransela17 <ransela17@gmail.com>
2021-01-18 18:02:35 +0200

    Blueprin project - yarin website without DB connection

--- web-project-10/components/main_menu/main_menu.py
+++ web-project-10/components/main_menu/main_menu.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, render_template
 
-# main_menu blueprint definition
-main_menu = Blueprint('main_menu', __name__, static_folder='static', static_url_path='/main_menu', template_folder='templates')
+# Header blueprint definition
+main_menu = Blueprint('Header', __name__, static_folder='static', static_url_path='/Header', template_folder='templates')
